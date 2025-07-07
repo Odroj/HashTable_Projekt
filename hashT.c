512706ae4080aef80a71a7f4cfbc8e1ad79e40e4
Odroj <davisjordyn06@gmail.com>
2025-07-06 22:49:53 -0500

    Finshed with hash functions

--- hashT.c
+++ hashT.c
@@ -58,10 +58,21 @@ void ht_insert(ht_hash_table*ht, const char* key, const char* value){
     int index=ht_get_hash(item->key, ht->size, 0);
     ht_item*cur_item=ht->items[index]; 
     int i=1; 
-    while(cur_item!=NULL){
-        index=ht_get_hash(item->key, ht->size, i);
-        cur_item=ht->items[index];
-        i++; 
+    while(cur_item!=NULL && cur_item!=&HT_DELETED_ITEM){
+        ht_item* item=ht_new_item(key, value); 
+        int index=ht_get_hash(key, ht->size, 0);
+        ht_item*cur_item=ht->items[index]; 
+        int i=1; 
+        while(cur_item!=NULL && cur_item!=&HT_DELETED_ITEM){
+            if(strcmp(cur_item->key, key)==0){
+                ht_del_item(cur_item);
+                ht->items[index]=item;
+                return;
+            }
+            index=ht_get_hash(key, ht->size, i);
+            cur_item=ht->items[index];
+            i++;
+        }
     }
     ht->items[index]=item;
     ht->count++;
